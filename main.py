5dbb55bff5fc3dab34ba1d779843685a5a51e1b1
Ben Weller <ben.m.weller@gmail.com>
2018-04-09 19:56:09 -0500

    I have gotten it to work as I want.

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+import pyperclip
+
+def create_mixed_word(word):
+    start_char = word[:1]
+    remainder = word[1:]
+    return start_char.upper() + remainder
+
+def create_mixed_phrase(phrase):
+    returned = ""
+    for x in phrase.split(" "):
+        returned += create_mixed_word(x) + " "
+    return returned.strip()
+
+def create_string(string, option):
+    if option == "lower":
+        return string.lower()
+    elif option == "mixed":
+        return create_mixed_phrase(string)
+    else:
+        return string.upper()
+
+if __name__ == "__main__":
+    string = pyperclip.paste()
+    if len(sys.argv) != 2:
+        print("Usage: stringOption")
+        print("Valid options: lower, mixed, upper.")
+    elif string == "":
+        print("You must have a string on your clipboard to be changed.")
+
+    pyperclip.copy(create_string(string, sys.argv[1]))
